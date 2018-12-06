d4549d3a0208efcd1d12fac6d4e7752e9354d87c
44021987 <44021987@qq.com>
2018-12-06 14:54:48 +0800

    add build

---
+++ build/precache-manifest.52a4f24be8cde4bc003f02bbaf07debd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e74cc23154ddeb717d1",
+    "url": "/static/js/main.4e74cc23.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "4e74cc23154ddeb717d1",
+    "url": "/static/css/main.f61f3709.chunk.css"
+  },
+  {
+    "revision": "b4c0e234f94fd1edce1b1f2cbbf22629",
+    "url": "/index.html"
+  }
+];
